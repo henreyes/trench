f28bb0a56c7bbff37647a4c2abcecc2147d2c46d
Henry Reyes <reyesh2@uci.edu>
2024-02-03 17:17:23 -0800

    repl basic function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut input = String::new();
+
+    loop {
+        print!("lisp> "); 
+        io::stdout().flush().unwrap();
+
+        input.clear(); 
+        match stdin.read_line(&mut input) {
+            Ok(n) => {
+                if n == 0 { 
+                    break;
+                }
+                let input_trimmed = input.trim(); 
+                if input_trimmed.is_empty() {
+                    continue;
+                }
+                println!("You entered: {}", input_trimmed); 
+            },
+            Err(error) => {
+                println!("Error reading input: {}", error); 
+                break;
+            },
+        }
+    }
 }
