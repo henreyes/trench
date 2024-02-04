1fbe5af9fadf5f547a92348e75165cd73ce0f128
Henry Reyes <reyesh2@uci.edu>
2024-02-03 22:26:26 -0800

    tokenize functionality

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,82 @@
-use std::io::{self, Write};
 
-fn main() {
-    let stdin = io::stdin();
-    let mut input = String::new();
+use core::fmt;
 
-    loop {
-        print!("lisp> "); 
-        io::stdout().flush().unwrap();
+#[derive(Debug, Clone, PartialEq)]
+pub enum Token {
+    OpenParen,
+    CloseParen,
+    Symbol(String),
+    Number(f64),
+}
+pub fn tokenize(input: &str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    let mut current_token = String::new();
 
-        input.clear(); 
-        match stdin.read_line(&mut input) {
-            Ok(n) => {
-                if n == 0 { 
-                    break;
-                }
-                let input_trimmed = input.trim(); 
-                if input_trimmed.is_empty() {
-                    continue;
-                }
-                println!("You entered: {}", input_trimmed); 
+    let mut chars = input.chars().peekable();
+    while let Some(&c) = chars.peek() {
+        match c {
+            '(' => {
+                tokens.push(Token::OpenParen);
+                chars.next();
             },
-            Err(error) => {
-                println!("Error reading input: {}", error); 
-                break;
+            ')' => {
+                tokens.push(Token::CloseParen);
+                chars.next();
             },
+            ' ' | '\n' | '\t' => {
+                chars.next();
+            },
+            _ => { 
+                while let Some(&nc) = chars.peek() {
+                    if nc == '(' || nc == ')' || nc.is_whitespace() {
+                        break;
+                    } else {
+                        current_token.push(nc);
+                        chars.next();
+                    }
+                }
+                if let Ok(n) = current_token.parse::<f64>() {
+                    tokens.push(Token::Number(n));
+                } else {
+                    tokens.push(Token::Symbol(current_token.clone()));
+                }
+                current_token.clear();
+            }
         }
     }
+
+    tokens
+}
+
+struct TokenVec(Vec<Token>);
+
+impl fmt::Display for TokenVec {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for (index, token) in self.0.iter().enumerate() {
+            if index > 0 { write!(f, " ")?; } 
+            write!(f, "{}", token)?;
+        }
+        Ok(())
+    }
+}
+
+impl fmt::Display for Token {
+fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+        Token::OpenParen => write!(f, "("),
+        Token::CloseParen => write!(f, ")"),
+        Token::Symbol(s) => write!(f, "{}", s),
+        Token::Number(n) => write!(f, "{}", n),
+    }
+}
+}
+
+fn main() {
+    let input = "(cond
+        ((> 5 10) 'greater)
+        ((< 5 10) 'lesser)
+        (t 'equal))";
+    let tokens = tokenize(input);
+    let token_vec = TokenVec(tokens); 
+    println!("{}", token_vec);
 }
