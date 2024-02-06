986dce0b532c294ecb06a6bc292e9ed1c3e59580
Henry Reyes <reyesh2@uci.edu>
2024-02-05 20:32:47 -0800

    start eval/placeholders

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,40 @@ use crate::parser::*;
 use crate::tokenize::*;
 
 
+pub fn assoc(symbol: &str) -> Result<Atom, String> {
+    println!("assoc to be defined");
+    Ok(Atom::Void)
+}
 
+pub fn apply_atom(list: &[Atom]) -> Result<Atom, String> {
+    if let Some(Atom::Symbol(s)) = list.first() {
+        match s.as_str() {
+            "+" => println!("add symbol"), 
+            "-" => println!("subtract symbol"),
+            "*" => println!("multiply symbol"),
+            "defun" => println!("defun symbol"),
+            "cond" => println!("cond symbol"),
+            _ => println!("nothing here")
+        }
+        Ok(Atom::Void)  
+    } else {
+        Err("Expected a symbol".to_string())
+    }
+}
 
+pub fn eval(parsed: &Atom) -> Result<Atom, String> {
+    match parsed {
+        Atom::List(expr) => apply_atom(expr),
+        Atom::Symbol(atom) => assoc(atom),
+        Atom::Integer(int) => Ok(Atom::Integer(*int)),
+        _ => Err("Unhandled Atom variant".to_string()),
+    }
+}
 
 
 fn main() {
     // "(defun my_function (x) (+ x 1))"
-    let input = "(1 2 3 4 5)";
+    let input = "(+ 1 5)";
     let tokens = tokenize(input);
     println!("Tokens: {:?}", tokens); 
 
@@ -23,4 +50,6 @@ fn main() {
         },
         Err(e) => println!("Parsing Error: {}", e),
     }
+
+
 }
