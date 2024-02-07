c194dfa99bc8797265a4ed1b8c9d9bc802663120
Henry Reyes <reyesh2@uci.edu>
2024-02-06 20:57:16 -0800

    fixed arith binding bug

--- src/eval.rs
+++ src/eval.rs
@@ -10,38 +10,36 @@ pub fn assoc(symbol: &str, a_list:  &Rc<RefCell<AList>>) -> Result<Atom, String>
     }
 }
 
-pub fn apply_atom(list: &[Atom]) -> Result<Atom, String> {
+pub fn apply_atom(list: &[Atom], a_list: &Rc<RefCell<AList>>) -> Result<Atom, String> {
     if let Some(Atom::Symbol(s)) = list.first() {
         match s.as_str() {
             "+" | "-" | "*" => {
                 if list.len() < 3 {
                     return Err("Not enough arguments".to_string());
                 }
-
+    
                 let mut ops = Vec::new();
                 for atom in &list[1..] {
-                    match atom {
-                        Atom::Integer(n) => ops.push(*n),
-                        _ => return Err("unsupported operand for operation".to_string()),
+                    let evaluated_atom = eval(atom, a_list)?;
+                    match evaluated_atom {
+                        Atom::Integer(n) => ops.push(n), 
+                        _ => return Err(format!("Expected an integer, found {:?}", evaluated_atom)),
                     }
                 }
-
-                let result = match s.as_str() {
+    
+                let result: f64 = match s.as_str() {
                     "+" => ops.iter().sum(),
                     "-" => ops.iter().skip(1).fold(ops[0], |acc, &val| acc - val),
                     "*" => ops.iter().product(),
                     _ => unreachable!(), 
                 };
-
                 return Ok(Atom::Integer(result));
-
-
             },
-            "defun" => println!("defun symbol"),
-            "cond" => println!("cond symbol"),
-            _ => println!("nothing here")
+            "defun" => return Ok(Atom::Void),
+            
+            "cond" =>  return Ok(Atom::Void),
+            _ => return Ok(Atom::Void),
         }
-        Ok(Atom::Void)  
     } else {
         Err("Expected a symbol".to_string())
     }
@@ -49,7 +47,7 @@ pub fn apply_atom(list: &[Atom]) -> Result<Atom, String> {
 
 pub fn eval(parsed: &Atom, a_list: &Rc<RefCell<AList>>) -> Result<Atom, String> {
     match parsed {
-        Atom::List(expr) => apply_atom(expr),
+        Atom::List(expr) => apply_atom(expr, a_list),
         Atom::Symbol(atom) => assoc(atom, a_list),
         Atom::Integer(int) => Ok(Atom::Integer(*int)),
         _ => Err("Unhandled Atom variant".to_string()),
