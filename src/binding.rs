5e8cbcb4d2386a3587ec418b907d5d99cff7c476
Henry Reyes <reyesh2@uci.edu>
2024-02-07 16:43:31 -0800

    parent alist for lexical scoping

--- src/binding.rs
+++ src/binding.rs
@@ -1,24 +1,39 @@
 use crate::parser::*;
 use std::cell::RefCell;
 use std::collections::HashMap;
+use std::rc::Rc;
 
 #[derive(Clone, Debug)]
 pub struct AList {
     bindings: RefCell<HashMap<String, Atom>>,
+    parent: Option<Rc<RefCell<AList>>>, 
 }
 
 impl AList {
     pub fn new() -> Self {
         AList {
             bindings: RefCell::new(HashMap::new()),
+            parent: None, 
         }
     }
 
-    pub fn add_binding(&self, key: String, value: Atom) {
-        self.bindings.borrow_mut().insert(key, value);
+    pub fn new_with_parent(parent: &Rc<RefCell<Self>>) -> Rc<RefCell<AList>> {
+        Rc::new(RefCell::new(AList {
+            bindings: RefCell::new(HashMap::new()),
+            parent: Some(parent.clone())
+        }))
     }
 
     pub fn get_binding(&self, key: &str) -> Option<Atom> {
-        self.bindings.borrow().get(key).cloned()
+        self.bindings.borrow().get(key).cloned().or_else(|| {
+            self.parent
+                .as_ref()
+                .and_then(|parent| parent.borrow().get_binding(key))
+        })
+    }
+
+    pub fn set_binding(&self, key: String, value: Atom) {
+        self.bindings.borrow_mut().insert(key, value);
     }
 }
+
